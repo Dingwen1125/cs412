cc999e5581e286bda26f2b55ee5ed5595b48d4a2
Dingwen1125 <laoba@bu.edu>
2026-02-12 21:08:32 -0500

    add comment

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -8,11 +8,17 @@ from .models import Profile
 # Create your views here.
 
 class ShowAllView(ListView):
+    '''
+    show all the item in the Profile
+    '''
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
 
 class ProfileDetailView(DetailView):
+    '''
+    show one item in Profile
+    '''
     model = Profile
     template_name = "mini_insta/show_profile.html"
     context_object_name = "profile"
